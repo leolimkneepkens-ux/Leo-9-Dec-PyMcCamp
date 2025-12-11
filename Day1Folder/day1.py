67abebba63685d0576e92c0e9e591e2ecb187731
leolimkneepkens-ux <leo_limkneepkens@students.edu.sg>
2025-12-11 06:23:51 +0000

    Dec 11, 2025, 6:23 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,39 +1,63 @@
-# Write all your codes for Day 1 here.
-# COMMENT out the previous task before going on to the next task
-print("hello from day1")
+
 
 ########################################################################
 # Task 1:
 
 
 
-########################################################################
-# Task 2:
+
+## ##################################################################### 
+# print("leo")
+# print("10") 
+# print("canossa catholic primary school") 
+# print("I would buy a few lego minecraft sets and save the rest")
 
 
 
 ########################################################################
-# Task 3:
-
+# Task 3: 
+# animal1 = "lion"
+# animal2 = "tiger"
+# animal3 = "dog"
+# print(animal1)
+# print(animal2) 
+# print(animal3)
 
 
 ########################################################################
 # Task 4:
-
-
+# variable = 310
+# variable = 512300
+# print(variable1 + variable2)
 
 ########################################################################
 # Task 5:
+# name = "Leo"
+# food = "bone"
+# print("my name is " + name + ".") 
+# print=("I like to eat")
 
 
 
 ########################################################################
 # Task 6:
 
+def selfintro():
+    print("my name is leo")
+    print("I am 67 years old") 
+    print("I attend canossa catholic primary school")
+    print("If I had $2000 I would save it")
 
+selfintro()
 
 ########################################################################
 # Task 7:
+
+# def addition(number1, number2):
+#     print(number1 + number2)
+
+# addition(10,20) 
+# addition(111, 789)
 
 
 
