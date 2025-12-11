5f7ea4f539cb1a63d13f11549c5f4ccd2744d3a1
leolimkneepkens-ux <leo_limkneepkens@students.edu.sg>
2025-12-11 06:28:42 +0000

    Dec 11, 2025, 6:28 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -5,7 +5,8 @@ print("hello from day3")
 ########################################################################
 # Task 1:
 Name = input("what is your name?") 
-title = input("what is your title?")
+title = input("what is your title?") 
+commend
 
 
 ########################################################################
